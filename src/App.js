8d4afa3015b8976a47eba9a29484b9081df7b9a6
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-11-02 17:35:48 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title:'Car Insurance',
+      date:new Date(2023,10,1),
+      amount:500,
+      place:'Noida'
+    },
+    {
+      title:'Hotel',
+      date:new Date(2023,9,20),
+      amount:55,
+      place:'Greater Noida'
+    },
+    {
+      title:'Food',
+      date:new Date(2023,9,28),
+      amount:25,
+      place:'Dadri'
+    },
+    {
+      title:'Water',
+      date:new Date(2023,9,29),
+      amount:2,
+      place:'Delhi'
+    }
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map(e=><ExpenseItem title={e.title} date={e.date} amount={e.amount} place={e.place}></ExpenseItem>)}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,22 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 10, 1);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 24000;
-    const expenseLocaton = 'Noida';
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US',{month:'long'});
+    const day = props.date.toLocaleString('en-US',{month:'2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>Rs. {expenseAmount}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
             </div>
-            <div>{expenseLocaton}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>Rs. {props.amount}</div>
+            </div>
+            <div>{props.place}</div>
         </div>
     );
 }
