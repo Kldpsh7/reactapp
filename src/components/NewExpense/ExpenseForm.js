22441dbd02dc4b2cab7f7088e226b138b59994ff
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-12-16 19:24:08 +0530

    Using state in new componant

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,28 +1,44 @@
+import { useState } from 'react';
 import React from "react";
 import './ExpenseForm.css'
 
 const ExpenseForm = ()=>{
-    const changeHandler = e=>{
-        console.log(e.target.value)
+    const [enteredTitle,setTitle] = useState('');
+    const [enteredAmount,setAmount] = useState('');
+    const [enteredDate,setDate] = useState('');
+    const [enteredPlace,setPlace] = useState('');
+
+    const titleChangeHandler = e=>{
+        setTitle(e.target.value);
     }
+    const amountChangeHandler = e=>{
+        setAmount(e.target.value);
+    }
+    const dateChangeHandler = e=>{
+        setDate(e.target.value);
+    }
+    const placeChangeHandler = e=>{
+        setPlace(e.target.value);
+    }
+    
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={changeHandler}/>
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" onChange={changeHandler}/>
+                    <input type="number" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="Date" onChange={changeHandler}/>
+                    <input type="Date" onChange={dateChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Place</label>
-                    <input type="text" onChange={changeHandler}/>
+                    <input type="text" onChange={placeChangeHandler}/>
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit" >Add Expense</button>
