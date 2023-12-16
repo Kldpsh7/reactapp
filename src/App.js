4134950802b425ef2dc8bbe2fb07f823838b965a
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-12-16 21:35:42 +0530

    Keys added to list items

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import { useState } from "react";
 const App = () => {
   const [expenses,updateExpenses] = useState([
     {
+      id:1,
       title:'Car Insurance',
       date:new Date(2023,10,1),
       amount:500,
       place:'Noida'
     },
     {
+      id:2,
       title:'Hotel',
       date:new Date(2023,9,20),
       amount:55,
       place:'Greater Noida'
     },
     {
+      id:3,
       title:'Food',
       date:new Date(2023,9,28),
       amount:25,
       place:'Dadri'
     },
     {
+      id:4,
       title:'Water',
       date:new Date(2023,9,29),
       amount:2,
@@ -31,16 +35,15 @@ const App = () => {
   ]);
 
   const newExpenseHandler = expenseData=>{
-      console.log(expenseData)
       updateExpenses(prevData=>{
-        return ([...prevData,expenseData])
+        return ([expenseData,...prevData])
       })
   }
   return (
     <div>
       <NewExpense onNewExpense={newExpenseHandler}/>
       <h2>Let's get started!</h2>
-      {expenses.map(e=><ExpenseItem title={e.title} date={e.date} amount={e.amount} place={e.place}/>)}
+      {expenses.map(e=><ExpenseItem key={e.id} title={e.title} date={e.date} amount={e.amount} place={e.place}/>)}
     </div>
   );
 }
