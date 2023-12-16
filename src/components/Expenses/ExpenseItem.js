be6e3daf78fde31a4b979bf2e9bdc18c316fc057
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-12-16 14:57:34 +0530

    Titile and amount update button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,32 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+
+    const [title,setTitle] = useState(props.title);
+    const [amount,setAmount] = useState(props.amount);
     
-    const clickHandler = e=>{
+    const deleteItem = e=>{
         e.target.parentElement.remove()
+    }
+
+    const updateTitle = ()=>{
+        setTitle('Updated');
+    }
+
+    const make100 = ()=>{
+        setAmount(100)
     }
 
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} amount={props.amount} place={props.place} />
-            <button onClick={clickHandler}>Delete</button>
+            <ExpenseDetails title={title} amount={amount} place={props.place} />
+            <button onClick={updateTitle}>Update Title</button>
+            <button onClick={make100}>$100</button>
+            <button onClick={deleteItem}>Delete</button>
         </div>
     );
 }
